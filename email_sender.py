947b6ac2d1b96b702952a1840d64ac4cb628d10d
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2023-04-11 22:35:35 +0530

    Create email_sender.py

---
+++ email_sender.py
@@ -0,0 +1,12 @@
+import smtplib
+
+# list of email_ids to send the mail to
+emails = ["random@gmail.com", "your.email@gmail.com"]
+
+for email in emails:
+	s = smtplib.SMTP('smtp.gmail.com', 587)
+	s.starttls()
+	s.login("sender_email_id", "sender_email_id_password")
+	message = "Message_you_need_to_send"
+	s.sendmail("sender_email_id", email, message)
+	s.quit()
