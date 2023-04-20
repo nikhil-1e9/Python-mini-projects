bc259571938da52d60a68148945911f06f8a965c
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2023-04-21 00:23:09 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,12 +1,18 @@
-import smtplib
+import smtplib 
+from email.message import EmailMessage
 
-# list of email_ids to send the mail to
-emails = ["random@gmail.com", "your.email@gmail.com"]
+# Creating a object for EmailMessage
+email = EmailMessage() 
+email['from'] = 'sender id'
+email['to'] = 'receiver id'
+email['subject'] = 'subject'  # Subject of email
+email.set_content("content of email") # content of email
 
-for email in emails:
-	s = smtplib.SMTP('smtp.gmail.com', 587)
-	s.starttls()
-	s.login("sender_email_id", "sender_email_id_password")
-	message = "Message_you_need_to_send"
-	s.sendmail("sender_email_id", email, message)
-	s.quit()
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:     
+## sending request to server 
+    
+    smtp.ehlo()          # server object
+    smtp.starttls()      # send data between server and client
+    smtp.login("email_id", "Password") # login id and password of gmail
+    smtp.send_message(email)   # Sending email
+    print("email sent successfully")    # success message
