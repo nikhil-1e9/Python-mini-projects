511f76e4ebb225e158e9f4610e4c3970aa3eb1c9
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2023-04-21 23:54:56 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,42 @@
+import random
+
+def pick_word():
+    words = ["python", "java", "html", "javascript", "csharp"]
+    return random.choice(words)
+
+def display_word(word, guesses):
+    display = ''
+    for letter in word:
+        if letter in guesses:
+            display += letter
+        else:
+            display += '-'
+    return display
+
+def get_guess(guesses):
+    while True:
+        guess = input("Guess a letter: ")
+        if guess in guesses:
+            print("You already guessed that letter. Try again.")
+        else:
+            return guess
+
+word = pick_word()
+guesses = []
+max_attempts = 10
+
+while True:
+    print(display_word(word, guesses))
+    guess = get_guess(guesses)
+    guesses.append(guess)
+    if guess in word:
+        print("Correct!")
+    else:
+        print("Incorrect.")
+        max_attempts -= 1
+    if max_attempts == 0:
+        print("You ran out of attempts. The word was", word)
+        break
+    if set(word) == set(guesses):
+        print("Congratulations! You guessed the word", word)
+        break
