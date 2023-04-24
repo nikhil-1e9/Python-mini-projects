aa95b97c4079ee87857c197619d50a77c59db380
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2023-04-24 22:17:37 +0530

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,25 +1,38 @@
+from bs4 import BeautifulSoup 
 import requests
-from bs4 import BeautifulSoup
-import csv
 
-url = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-tables = soup.find_all('table')
+def google(query):
+    query = query.replace(" ", "+")
+    try:
+        url = f'https://www.google.com/search?q={query}&oq={query}&aqs=chrome..69i57j46j69i59j35i39j0j46j0l2.4948j0j7&sourceid=chrome&ie=UTF-8'
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+    except:
+        print("Make sure you have an active internet connection")
+    try:
+        try:
+            ans = soup.select('.RqBzHd')[0].getText().strip()
+        
+        except:
+            try:
+                title=soup.select('.AZCkJd')[0].getText().strip()
+                try:
+                    ans=soup.select('.e24Kjd')[0].getText().strip()
+                except:
+                    ans=""
+                ans=f'{title}\n{ans}'
+                
+            except:
+                try:
+                    ans=soup.select('.hgKElc')[0].getText().strip()
+                except:
+                    ans=soup.select('.kno-rdesc span')[0].getText().strip()
+    
+    except:
+        ans = "can't find on google"
+    return ans
 
-data = []
-
-for table in tables:
-    rows = table.find_all('tr')
-    for row in rows:
-        cells = row.find_all('td')
-        if len(cells) > 0:
-            row_data = []
-            for cell in cells:
-                row_data.append(cell.text.strip())
-            data.append(row_data)
-
-with open('data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(data)
+result = google(str(input("Query\n")))
+print(result)    
